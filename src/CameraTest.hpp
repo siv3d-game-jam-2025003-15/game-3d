a49404aeca8f7d7da8b529cd76688687f231b386
massie0414 <massie0414@gmail.com>
2025-03-15 14:45:53 +0900

    Rayの仮実装

--- src/CameraTest.hpp
+++ src/CameraTest.hpp
@@ -12,6 +12,39 @@ public:
 
 	void draw() const override;
 
+	Ray getMouseRay() const
+	{
+		//return m_camera.screenToRay(Cursor::PosF());
+		return camera.screenToRay(Cursor::PosF());
+	}
+
 private:
 
+	// 縦方向の視野角（ラジアン）
+	//double m_verticalFOV = 25_deg;
+
+	// カメラの視点の位置
+	//Vec3 m_eyePosition{ 16, 16, -16 };
+
+	// カメラの注視点の位置
+	//Vec3 m_focusPosition{ 0, 0, 0 };
+
+	//double m_phi = -20_deg;
+
+	//double m_theta = 50_deg;
+
+	//BasicCamera3D m_camera;
+	DebugCamera3D camera;
+
+	Size SceneSize{ 256, 192 };
+
+	Model blacksmithModel{ U"" };
+
+	MSRenderTexture renderTexture;
+
+	ColorF backgroundColor;
+
+	int count = 0;
+
+	Audio audio;
 };
